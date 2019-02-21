1c943acdf3a15c6168e4c65f09079aa704a55ea0
Dusan <masinacdt@gmail.com>
2019-02-21 14:55:52 +0100

    added AS keyword (alias in namespace)

--- calling_ns.php
+++ calling_ns.php
@@ -1,29 +1,45 @@
+<html>
+<body>
 <?php
 
 //Koristi se sledeci namespace:
 use DutjaProject\Wild;
+use DutjaProject\Wild\Decak as Decko;
+
+class Decak
+{
+    private $ok;
+
+    public function __construct()
+    {
+        $this->ok = true;
+    }
+}
 
 require_once("D:/xampp/htdocs/symphart/public/namespaces.php");
 //header('Content-type: text/plain');
 
-$niz = array(5, 4, 7, 2, 6, 4, 9, 1);
+$niz=array(5,4,7,2,6,4,9,1);
 
 //(Konstante su boundovane za namespace, varijable ne);
 //Konstanta u namespace-u mora biti deklarisana sa "const"
 echo Wild\DUSKO."<br>";
 
 //Funkcija sort je iz drugog namespace-a, vraca sumu niza
-$d = Wild\sort($niz);
-echo $d;
+$d=Wild\sort($niz);
+//echo $d;
 
 //Pozivanje metoda iz 2. namespace-a i kreiranje objekata
-Wild\Decak::broj_decaka_init();
-$duki = new Wild\Decak(4);
-$vasa = new Wild\Decak(3);
-$sima = new Wild\Decak(2);
-$milan = new Wild\Decak(3);
+Decko::broj_decaka_init();
+$duki=new Decko(4);
+$vasa=new Decko(3);
+$sima=new Decko(2);
+$milan=new Decko(3);
+//$ivan=new Decko(2);
 
-echo $vasa->getGodine();
-echo Wild\Decak::getBrDecaka();
+echo $vasa->getGodine()."<br>";
+echo Decko::getBrDecaka()."<br>";
 echo dux;
  ?>
+</body>
+</html>
